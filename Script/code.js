a18723835e02c71967ce3dc275e86c04d8501169
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-06-06 09:18:09 +0200

    done

---
+++ Script/code.js
@@ -0,0 +1,14 @@
+let Answer = document.getElementById("Output")
+let Display = document.getElementById("Seconds")
+let SetMin = [8, 5, 3, 2, 4, 89, 78]
+Minimum = Math.min(...SetMin)
+Maximum = Math.max(...SetMin)
+function ArrayMinMax() {
+    
+    console.log(SetMin)
+console.log("The minimum Number is")
+console.log(Minimum)
+console.log("The Maximum number is")
+console.log(Maximum)
+
+}
